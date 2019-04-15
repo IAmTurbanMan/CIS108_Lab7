528497e972085060c4e48dd01ede122e198c5d6a
Rashidul Kader <46700922+IAmTurbanMan@users.noreply.github.com>
2019-04-15 14:51:29 -0700

    added the actual files

---
+++ Exercise1/MusicDB.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "Song.h"
+
+using namespace std;
+
+void programLoad(string fileName);
+
+void addSong();
+
+void saveSong(metadata::Song& s, string fileName);
+
+void listSong();
+
+void clearSong(string fileName);
+
+void sortYear();
+
+void sortArtist();
+
+void sortTitle();
+
+//void findSong();
+
+//bool songSearch(metadata::Song& search, string s);
+
+void menu();
